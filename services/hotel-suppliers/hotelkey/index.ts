6e7b0849ac0635993d0686767d16e16a228c5d09
IratePilotAdmin <ceo@iratepilot.com>
2026-08-07 15:34:33 -0500

    Add HotelKey integration foundation

---
+++ services/hotel-suppliers/hotelkey/index.ts
@@ -0,0 +1,14 @@
+export { HotelKeyAdapter } from "./adapter";
+export type {
+  HotelKeyAvailabilityRequest,
+  HotelKeyCancellation,
+  HotelKeyCancelReservationRequest,
+  HotelKeyCreateReservationRequest,
+  HotelKeyMapper,
+  HotelKeyOffer,
+  HotelKeyOperation,
+  HotelKeyReservation,
+  HotelKeyStay,
+  HotelKeyTransport,
+  HotelKeyTransportRequest,
+} from "./contracts";
