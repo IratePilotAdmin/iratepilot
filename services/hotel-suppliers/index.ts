56c175e34d0d029dbae868a1c87237820d2fcbd0
IratePilotAdmin <ceo@iratepilot.com>
2026-08-07 15:12:09 -0500

    Export Hilton PMS foundation

--- services/hotel-suppliers/index.ts
+++ services/hotel-suppliers/index.ts
@@ -1,6 +1,21 @@
 export { mockSupplier } from "./mock";
 export { getPmsProvider, pmsProviders } from "./providers";
 export { buildPmsReadiness } from "./readiness";
+export { HiltonPmsAdapter } from "./hilton";
+export type {
+  HiltonAvailabilityRequest,
+  HiltonCancellation,
+  HiltonCancelReservationRequest,
+  HiltonCreateReservationRequest,
+  HiltonOffer,
+  HiltonOperation,
+  HiltonPmsMapper,
+  HiltonPmsProvider,
+  HiltonPmsTransport,
+  HiltonReservation,
+  HiltonStay,
+  HiltonTransportRequest,
+} from "./hilton";
 export {
   loadOracleOperaConfig,
   OracleOperaClient,
