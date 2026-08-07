ac58812921d475147486d68b2834bae88ed2d940
IratePilotAdmin <ceo@iratepilot.com>
2026-08-06 22:39:40 -0500

    feat(mobile): provide authentication across routes

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -3,11 +3,13 @@ import "react-native-gesture-handler";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
+import { AuthProvider } from "@/providers/AuthProvider";
+
 export default function RootLayout() {
   return (
-    <>
+    <AuthProvider>
       <Stack screenOptions={{ headerShown: false }} />
       <StatusBar style="auto" />
-    </>
+    </AuthProvider>
   );
 }
