cd297299fe229814c41254475e54445936061c5d
IratePilotAdmin <ceo@iratepilot.com>
2026-08-07 15:22:15 -0500

    Export Marriott PMS adapter

---
+++ services/hotel-suppliers/marriott/index.ts
@@ -0,0 +1,7 @@
+export { MarriottPmsAdapter } from "./adapter";
+export type {
+  MarriottAvailabilityRequest, MarriottCancellation, MarriottCancelReservationRequest,
+  MarriottCreateReservationRequest, MarriottOffer, MarriottOperation, MarriottPmsMapper,
+  MarriottPmsProvider, MarriottPmsTransport, MarriottReservation, MarriottStay,
+  MarriottTransportRequest,
+} from "./contracts";
