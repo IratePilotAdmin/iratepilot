881824e24e1914d4bdd259c52374c722b8989fe2
IratePilotAdmin <ceo@iratepilot.com>
2026-08-07 14:54:21 -0500

    Export OPERA Cloud contracts

--- services/hotel-suppliers/oracle-opera/index.ts
+++ services/hotel-suppliers/oracle-opera/index.ts
@@ -1,4 +1,16 @@
 export { OracleOperaClient, OracleOperaClientError } from "./client";
 export type { OracleOperaRequestOptions } from "./client";
+export { OracleOperaAdapter } from "./adapter";
 export { loadOracleOperaConfig } from "./config";
 export type { OracleOperaConfig } from "./config";
+export type {
+  OperaAvailabilityRequest,
+  OperaCancelReservationRequest,
+  OperaCancellation,
+  OperaCreateReservationRequest,
+  OperaGuest,
+  OperaOffer,
+  OperaReservation,
+  OperaStay,
+  OracleOperaContractMapper,
+} from "./contracts";
