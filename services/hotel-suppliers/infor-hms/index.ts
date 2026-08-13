2b11130851d743c3028b689bad0d7c2f53f51483
IratePilotAdmin <ceo@iratepilot.com>
2026-08-13 11:52:34 -0500

    Export Infor HMS synchronization adapter

--- services/hotel-suppliers/infor-hms/index.ts
+++ services/hotel-suppliers/infor-hms/index.ts
@@ -1,2 +1,6 @@
 export { InforHmsHttpTransport, InforHmsTransportError } from "./transport";
+export { InforHmsBookingMapper } from "./mapper";
+export { createInforHmsSyncAdapter, loadInforHmsSyncConfig } from "./sync";
 export type { InforHmsConfig, InforHmsEndpoint, InforHmsFetch } from "./transport";
+export type { InforHmsBookingMapperConfig } from "./mapper";
+export type { InforHmsSyncEnvironment } from "./sync";
