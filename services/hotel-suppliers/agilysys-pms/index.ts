b55b4e815e78885f52f2ada24d02946d7e69ea94
IratePilotAdmin <ceo@iratepilot.com>
2026-08-13 11:57:49 -0500

    Export Agilysys PMS synchronization adapter

--- services/hotel-suppliers/agilysys-pms/index.ts
+++ services/hotel-suppliers/agilysys-pms/index.ts
@@ -1,2 +1,6 @@
 export { AgilysysPmsHttpTransport, AgilysysPmsTransportError } from "./transport";
+export { AgilysysPmsBookingMapper } from "./mapper";
+export { createAgilysysPmsSyncAdapter, loadAgilysysPmsSyncConfig } from "./sync";
 export type { AgilysysPmsConfig, AgilysysPmsEndpoint, AgilysysPmsFetch } from "./transport";
+export type { AgilysysPmsBookingMapperConfig } from "./mapper";
+export type { AgilysysPmsSyncEnvironment } from "./sync";
