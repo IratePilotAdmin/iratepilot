a535ecb2907523ce83ecc8350cc239ad74ba2c28
IratePilotAdmin <ceo@iratepilot.com>
2026-08-13 12:03:41 -0500

    Export Planet Protel synchronization adapter

--- services/hotel-suppliers/planet-protel/index.ts
+++ services/hotel-suppliers/planet-protel/index.ts
@@ -1,2 +1,6 @@
 export { PlanetProtelHttpTransport, PlanetProtelTransportError } from "./transport";
+export { PlanetProtelBookingMapper } from "./mapper";
+export { createPlanetProtelSyncAdapter, loadPlanetProtelSyncConfig } from "./sync";
 export type { PlanetProtelConfig, PlanetProtelEndpoint, PlanetProtelFetch } from "./transport";
+export type { PlanetProtelBookingMapperConfig } from "./mapper";
+export type { PlanetProtelSyncEnvironment } from "./sync";
