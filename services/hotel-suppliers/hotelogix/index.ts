fcc7f0852cf315ec598d3bed7cff2358d8ad6b34
IratePilotAdmin <ceo@iratepilot.com>
2026-08-13 11:46:27 -0500

    Export Hotelogix synchronization adapter

--- services/hotel-suppliers/hotelogix/index.ts
+++ services/hotel-suppliers/hotelogix/index.ts
@@ -1,4 +1,8 @@
 export { HotelogixHttpTransport, HotelogixTransportError } from "./transport";
+export { HotelogixBookingMapper } from "./mapper";
+export { createHotelogixSyncAdapter, loadHotelogixSyncConfig } from "./sync";
 export type {
   HotelogixConfig, HotelogixEndpoint, HotelogixFetch,
 } from "./transport";
+export type { HotelogixBookingMapperConfig } from "./mapper";
+export type { HotelogixSyncEnvironment } from "./sync";
