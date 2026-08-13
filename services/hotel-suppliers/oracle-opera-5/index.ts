1c854a217d120fe2fa5f73ca6462da135aa0d442
IratePilotAdmin <ceo@iratepilot.com>
2026-08-13 12:08:56 -0500

    Export Oracle OPERA 5 synchronization adapter

--- services/hotel-suppliers/oracle-opera-5/index.ts
+++ services/hotel-suppliers/oracle-opera-5/index.ts
@@ -1,4 +1,11 @@
 export { OracleOpera5Transport, OracleOpera5TransportError } from "./transport";
+export { OracleOpera5BookingMapper } from "./mapper";
+export { createOracleOpera5SyncAdapter, loadOracleOpera5SyncConfig } from "./sync";
 export type {
   OracleOpera5Config, OracleOpera5Endpoint, OracleOpera5Fetch, OracleOpera5SoapHeaders,
 } from "./transport";
+export type { OracleOpera5BookingMapperConfig } from "./mapper";
+export type {
+  OracleOpera5EnvelopeBuilder, OracleOpera5ResponseParser, OracleOpera5SoapHeaderProvider,
+  OracleOpera5SyncEnvironment,
+} from "./sync";
