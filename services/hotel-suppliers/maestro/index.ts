eaa2ba4f5f8f1cda0f35507690a3a17220430431
IratePilotAdmin <ceo@iratepilot.com>
2026-08-13 10:43:55 -0500

    Export Maestro booking synchronization

--- services/hotel-suppliers/maestro/index.ts
+++ services/hotel-suppliers/maestro/index.ts
@@ -2,3 +2,7 @@ export { MaestroHttpTransport, MaestroTransportError } from "./transport";
 export type { MaestroConfig, MaestroEndpoint, MaestroFetch } from "./transport";
 export { MaestroConnectionTestError, testMaestroSandboxConnection } from "./connection-test";
 export type { MaestroConnectionTestConfig, MaestroConnectionTestResult } from "./connection-test";
+export { MaestroBookingMapper } from "./mapper";
+export type { MaestroBookingMapperConfig } from "./mapper";
+export { createMaestroSyncAdapter, loadMaestroSyncConfig } from "./sync";
+export type { MaestroSyncEnvironment } from "./sync";
