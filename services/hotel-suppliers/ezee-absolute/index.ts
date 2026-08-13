35a0f30efaf6e5b1bd8725dc8c89ff191480b434
IratePilotAdmin <ceo@iratepilot.com>
2026-08-13 11:08:19 -0500

    Export eZee Absolute booking synchronization

--- services/hotel-suppliers/ezee-absolute/index.ts
+++ services/hotel-suppliers/ezee-absolute/index.ts
@@ -1,3 +1,7 @@
 export { EzeeAbsoluteHttpTransport, EzeeAbsoluteTransportError } from "./transport";
 export type { EzeeAbsoluteConfig, EzeeAbsoluteEndpoint, EzeeAbsoluteFetch } from "./transport";
+export { EzeeAbsoluteBookingMapper } from "./mapper";
+export type { EzeeAbsoluteBookingMapperConfig } from "./mapper";
+export { createEzeeAbsoluteSyncAdapter, loadEzeeAbsoluteSyncConfig } from "./sync";
+export type { EzeeAbsoluteSyncEnvironment } from "./sync";
 
