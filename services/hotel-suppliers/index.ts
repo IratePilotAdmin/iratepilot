8825d16097f6df4a9b2fe9d50b819463aae4bb8d
IratePilotAdmin <ceo@iratepilot.com>
2026-08-13 11:46:28 -0500

    Export Hotelogix synchronization adapter

--- services/hotel-suppliers/index.ts
+++ services/hotel-suppliers/index.ts
@@ -107,13 +107,8 @@ export {
 } from "./maestro";
 export { MaestroConnectionTestError, testMaestroSandboxConnection } from "./maestro";
 export type {
-  MaestroBookingMapperConfig,
-  MaestroConfig,
-  MaestroConnectionTestConfig,
-  MaestroConnectionTestResult,
-  MaestroEndpoint,
-  MaestroFetch,
-  MaestroSyncEnvironment,
+  MaestroConfig, MaestroConnectionTestConfig, MaestroConnectionTestResult,
+  MaestroBookingMapperConfig, MaestroEndpoint, MaestroFetch, MaestroSyncEnvironment,
 } from "./maestro";
 export { ShijiHttpTransport, ShijiTransportError } from "./shiji";
 export { ShijiBookingMapper, createShijiSyncAdapter, loadShijiSyncConfig } from "./shiji";
@@ -135,7 +130,11 @@ export type {
   ClockPmsEndpoint, ClockPmsFetch, ClockPmsSyncEnvironment,
 } from "./clock-pms-plus";
 export { HotelogixHttpTransport, HotelogixTransportError } from "./hotelogix";
-export type { HotelogixConfig, HotelogixEndpoint, HotelogixFetch } from "./hotelogix";
+export { HotelogixBookingMapper, createHotelogixSyncAdapter, loadHotelogixSyncConfig } from "./hotelogix";
+export type {
+  HotelogixBookingMapperConfig, HotelogixConfig, HotelogixEndpoint,
+  HotelogixFetch, HotelogixSyncEnvironment,
+} from "./hotelogix";
 export { InforHmsHttpTransport, InforHmsTransportError } from "./infor-hms";
 export type { InforHmsConfig, InforHmsEndpoint, InforHmsFetch } from "./infor-hms";
 export { AgilysysPmsHttpTransport, AgilysysPmsTransportError } from "./agilysys-pms";
